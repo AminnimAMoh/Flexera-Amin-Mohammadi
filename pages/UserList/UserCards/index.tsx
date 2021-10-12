08342135da216b2d3538acc1143939d8844086db
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-12 13:39:24 +1100

    Card component is styled a little.

--- pages/UserList/UserCards/index.tsx
+++ pages/UserList/UserCards/index.tsx
@@ -1,21 +1,18 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement } from "react";
 import { Item } from "../../../Types/generalTypes";
 import styles from "../../../styles/Cards.module.css";
-import Image from "next/image"
+import Image from "next/image";
 
-function UserCards({full_name, avatar_url, description}: Item): ReactElement {
-    return (
-        <div className={styles.container}>
-            <Image 
-            src={avatar_url} 
-            alt="Image_Content"
-            width="100%"
-            height="100%"
-            />
-            <h3>{full_name}</h3>
-            <h3>{description}</h3>
-        </div>
-    )
+function UserCards({ full_name, avatar_url, description }: Item): ReactElement {
+  return (
+    <div className={styles.container}>
+      <Image src={avatar_url} alt="Image_Content" width="100%" height="100%" />
+      <div className={styles.container_infoCards}>
+        <h3>{full_name}</h3>
+        <h3>{description}</h3>
+      </div>
+    </div>
+  );
 }
 
-export default UserCards
+export default UserCards;
